54e805297966a0ac2a5b39f3ed765238153a0732
David Chambers <david_chambers@esri.com>
2016-03-04 17:03:22 -0500

    query for user feature service

--- app/src/main/java/david/com/quartztraining/MainActivity.java
+++ app/src/main/java/david/com/quartztraining/MainActivity.java
@@ -8,14 +8,23 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.esri.arcgisruntime.concurrent.ListenableFuture;
 import com.esri.arcgisruntime.loadable.LoadStatus;
 import com.esri.arcgisruntime.mapping.Basemap;
 import com.esri.arcgisruntime.mapping.BasemapType;
 import com.esri.arcgisruntime.mapping.view.MapView;
 import com.esri.arcgisruntime.mapping.Map;
 import com.esri.arcgisruntime.portal.Portal;
+import com.esri.arcgisruntime.portal.PortalFolder;
+import com.esri.arcgisruntime.portal.PortalItem;
+import com.esri.arcgisruntime.portal.PortalItemType;
+import com.esri.arcgisruntime.portal.PortalUser;
+import com.esri.arcgisruntime.portal.PortalUserContent;
 import com.esri.arcgisruntime.security.Credential;
 import com.esri.arcgisruntime.security.UserCredential;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -28,6 +37,12 @@ public class MainActivity extends AppCompatActivity {
     private String mUsername;
     private String mPassword;
 
+    //Portal variable
+    private Portal essMaps;
+
+    //Array of portal Items
+    private ArrayList<PortalItem> myPortalItems;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,13 +51,14 @@ public class MainActivity extends AppCompatActivity {
         mUsername = getIntent().getExtras().getString("usernameKey");
         mPassword = getIntent().getExtras().getString("passwordKey");
 
-        final Portal essMaps = new Portal("http://ess.maps.arcgis.com", new UserCredential(mUsername, mPassword));
+        essMaps = new Portal("http://ess.maps.arcgis.com", new UserCredential(mUsername, mPassword));
         essMaps.loadAsync();
         essMaps.addDoneLoadingListener(new Runnable() {
             @Override
             public void run() {
                 if(essMaps.getLoadStatus() == LoadStatus.LOADED){
                     Toast.makeText(getApplicationContext(),"Portal loaded!",Toast.LENGTH_LONG).show();
+                    fetchUserContent();
                 } else {
                     runOnUiThread(new Runnable() {
                         @Override
@@ -74,5 +90,45 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+    }
+
+    public void fetchUserContent(){
+        final PortalUser user = essMaps.getPortalUser();
+
+        //fetch the content in the users root folder
+        final ListenableFuture<PortalUserContent> contentFuture = user.fetchContentAsync();
+        contentFuture.addDoneListener(new Runnable() {
+
+            @Override
+            public void run() {
+                try {
+                    StringBuilder userInfo = new StringBuilder();
+                    //iterate items in root folder...
+                    final PortalUserContent portalUserContent = contentFuture.get();
+
+                    //Instantiate ArrayList
+                    myPortalItems = new ArrayList<PortalItem>();
+
+                    for (PortalItem item : portalUserContent.getItems())
+                    {
+                        if(item.getType() == PortalItemType.FEATURE_SERVICE) {
+                            myPortalItems.add(item);
+                            userInfo.append(String.format("Item: %s\n", item.getTitle()));
+                        }
+
+
+                    }
+
+                    //iterate user's folders - maybe implement this in the future
+                    if(myPortalItems != null){
+                        for(PortalItem item : myPortalItems){
+                            Log.d("MyTag", item.getTitle());
+                        }
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
